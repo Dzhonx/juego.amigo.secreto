fd9f569be719dafe7c0a587ffa76dc924b666915
Jhon Ramirez Ledesma <jhonrale1402@gmail.com>
2025-09-13 00:19:04 -0500

    Actualizando app.js

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let arrayAmigos = [];
+
+function agregarAmigo(){
+    //Capturando el valor del input
+    let inputAmigo = (document.getElementById('amigo'));
+    let nombreAmigo = inputAmigo.value;
+
+    //Validando entrada
+    if(nombreAmigo == ''){
+        alert('Error: Por favor, inserte un nombre.');
+    } else {
+        //Agragando a la lista
+        arrayAmigos.push(nombreAmigo);
+
+        //Mostrando lista
+        let lista = document.getElementById("listaAmigos");
+        let fila = document.createElement("li");
+        fila.textContent = nombreAmigo;
+        lista.appendChild(fila);
+
+        //Limpiando el input
+        inputAmigo.value = "";
+        return;
+    }
+}
+
+function sortearAmigo(){
+    //.........................
+}
+
